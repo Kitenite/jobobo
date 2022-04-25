196947573f69d0e5d119a06983306a0afd563841
Kitenite <hoakiet98@gmail.com>
2022-04-25 13:23:18 -0700

    Update input responsive

--- src/components/EmailInput.tsx
+++ src/components/EmailInput.tsx
@@ -18,17 +18,20 @@ export default function EmailInput() {
 
   return (
     <div className="relative w-full max-w-lg p-10">
-      <form onSubmit={onSubmit}>
-        <input
-          type="text"
-          className="h-12 w-3/4 border-2 border-r-0 border-slate-400 pl-10 pr-20 hover:border-sky-600"
-          placeholder="Enter your email"
-        />
-        <input
-          className="h-12 w-1/4 border-2 border-l-0 border-slate-400 bg-green-600 text-white hover:bg-green-700"
-          type="submit"
-          value="Join waitlist"
-        />
+      <form className="w-full max-w-sm" onSubmit={onSubmit}>
+        <div className="flex items-center border-b border-teal-500 py-2">
+          <input
+            className="mr-3 w-full appearance-none border-none bg-transparent py-1 px-2 leading-tight text-gray-700 focus:outline-none"
+            type="text"
+            placeholder="Enter your email"
+            aria-label="email"
+          />
+          <input
+            className="flex-shrink-0 rounded border-4 border-teal-500 bg-teal-500 py-1 px-2 text-sm text-white hover:border-teal-700 hover:bg-teal-700"
+            type="button"
+            value="Join waitlist"
+          />
+        </div>
       </form>
     </div>
   );
